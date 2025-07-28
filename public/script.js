47907c1295523f5b2885a282ef865104d8388e64
HereNoEvil <herenoevil.contact@gmail.com>
2025-07-28 13:53:24 -0700

    Delete public directory
    delete

--- public/script.js
+++
@@ -1,9 +0,0 @@
-const faders = document.querySelectorAll('.fade-in');
-window.addEventListener('scroll', () => {
-  faders.forEach(fader => {
-    const rect = fader.getBoundingClientRect();
-    if (rect.top < window.innerHeight - 100) {
-      fader.classList.add('visible');
-    }
-  });
-});
